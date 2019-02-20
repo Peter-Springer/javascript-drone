4b5857346a2ba8cd72e77198c3dd9c34eb75517b
Peter Springer <peterspringer829@gmail.com>
2019-02-19 21:14:30 -0600

    connect client and server sockets

--- client/components/DroneController.js
+++ client/components/DroneController.js
@@ -1,5 +1,107 @@
-const DroneController = () => (
-  <p>Controller Component</p>
+import styled from 'styled-components';
+import socket from '../socket';
+
+const CommandGrid = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1.25fr 1fr;
+  grid-template-rows: repeat(3, 1fr);
+  border: 1px solid black;
+  button {
+    text-shadow: 2px 2px 0 rgba(0, 0, 0, 0.05);
+    border: 0;
+    background: #2f2825;
+    border: 4px solid black;
+    color: white;
+    font-size: 1rem;
+    position: relative;
+    &:active {
+      top: 2px;
+    }
+    &:focus {
+      outline: 0;
+      border-color: #ffc600;
+    }
+    &.takeoff {
+      background: #5664aa;
+    }
+    &.land {
+      background: #047304;
+    }
+    &.emergency {
+      background: red;
+      text-transform: uppercase;
+      color: black;
+    }
+    &.rotate {
+      background: #ff9100;
+      color: black;
+    }
+    &.height {
+      background: #fff;
+      color: black;
+    }
+    span.symbol {
+      display: block;
+      font-size: 2rem;
+      font-weight: 400;
+    }
+  }
+  .center {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    button:last-child {
+      grid-column: span 2;
+    }
+  }
+`;
+
+function sendCommand(command) {
+  return function() {
+    console.log(`Sending the command ${command}`);
+    socket.emit('command', command);
+  };
+}
+
+const amount = 100;
+const Commands = () => (
+  <CommandGrid>
+    <button className="rotate" onClick={sendCommand('ccw 90')}>
+      <span className="symbol">⟲</span> 90°
+    </button>
+    <button onClick={sendCommand(`forward ${amount}`)}>
+      <span className="symbol">↑</span> forward {amount}cm
+    </button>
+    <button className="rotate" onClick={sendCommand('cw 15')}>
+      <span className="symbol">⟳</span> 15°
+    </button>
+    <button onClick={sendCommand(`left ${amount}`)}>
+      <span className="symbol">←</span> left {amount}cm
+    </button>
+    <div className="center">
+      <button className="takeoff" onClick={sendCommand('takeoff')}>
+        Take Off
+      </button>
+      <button className="land" onClick={sendCommand('land')}>
+        Land
+      </button>
+      <button className="emergency" onClick={sendCommand('emergency')}>
+        !! EMERGENCY !!
+      </button>
+    </div>
+    <button onClick={sendCommand(`right ${amount}`)}>
+      <span className="symbol">→</span>
+      right {amount}cm
+    </button>
+    <button className="height" onClick={sendCommand(`up ${amount}`)}>
+      <span className="symbol">⤒</span> {amount}cm
+    </button>
+    <button onClick={sendCommand(`back ${amount}`)}>
+      <span className="symbol">↓</span> back {amount}cm
+    </button>
+    <button className="height" onClick={sendCommand(`down ${amount}`)}>
+      <span className="symbol">⤓</span> {amount}cm
+    </button>
+  </CommandGrid>
 );
 
-export default DroneController;
+export default Commands;
--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
 const dgram = require('dgram')
 const wait = require('waait')
 const commandDelays = require('./commandDelays')
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 const PORT = 8889;
 const HOST = '192.168.10.1'
@@ -8,11 +11,6 @@ const HOST = '192.168.10.1'
 // create socket connection via udp4 and bind to PORT
 const drone = dgram.createSocket('udp4')
 drone.bind(PORT)
-
-//listen to messages being sent back from drone
-drone.on('message', message => {
-  console.log(`🎉: ${message}`)
-})
 
 //error handler
 function handleError(err) {
@@ -22,27 +20,47 @@ function handleError(err) {
   }
 }
 
+drone.send('command', 0, 'command'.length, PORT, HOST, handleError);
+
+io.on('connection', socket => {
+  socket.on('command', command => {
+    console.log('command Sent from browser');
+    console.log(command);
+    drone.send(command, 0, command.length, PORT, HOST, handleError)
+
+  });
+});
+
+//listen to messages being sent back from drone
+drone.on('message', message => {
+  console.log(`🎉: ${message}`)
+})
+
 // enter command mode
 // drone.send('command', 0, 7, PORT, HOST, handleError)
 
 //send battery command, offset, length of command, port, host, callback
 // drone.send('battery?', 0, 8, PORT, HOST, handleError)
 
-const commands = ['command', 'battery?', 'takeoff', 'forward 120', 'back 120', 'land']
+// const commands = ['command', 'battery?', 'takeoff', 'forward 120', 'back 120', 'land']
+//
+// let i = 0
+//
+// async function fly() {
+//   const command = commands[i]
+//   const delay = commandDelays[command.split(' ')[0]]
+//   console.log(`running command: ${command}`)
+//   drone.send(command, 0, command.length, PORT, HOST, handleError)
+//   await wait(delay)
+//   i += 1
+//   if (i < commands.length) {
+//     return fly();
+//   }
+//   console.log('done flying!')
+// }
+//
+// fly()
 
-let i = 0
-
-async function fly() {
-  const command = commands[i]
-  const delay = commandDelays[command.split(' ')[0]]
-  console.log(`running command: ${command}`)
-  drone.send(command, 0, command.length, PORT, HOST, handleError)
-  await wait(delay)
-  i += 1
-  if (i < commands.length) {
-    return fly();
-  }
-  console.log('done flying!')
-}
-
-fly()
+http.listen(9999, () => {
+  console.log('Socket io server up and running');
+});
